0bc612cd1d09669b8063b690d537934996067eca
zrsnk <niall09130126@gmail.com>
2023-10-08 23:44:24 +0900

    dashboardページ作成

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,83 @@
+'use client'
+
+import { Card } from '@/components/ui/card'
+import {
+  ArrowRight,
+  MessageSquare,
+  Music,
+  ImageIcon,
+  VideoIcon,
+  Code,
+} from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { useRouter } from 'next/navigation'
+
+const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    color: 'text-violet-500',
+    bgColor: 'bg-violet-500/10',
+    href: '/conversation',
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+    href: '/music',
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    bgColor: 'bg-pink-700/10',
+    href: '/image',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    bgColor: 'bg-orange-700/10',
+    href: '/video',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: 'text-green-700',
+    bgColor: 'bg-green-700/10',
+    href: '/code',
+  },
+]
+
 export default function DashboardPage() {
+  const router = useRouter()
+
   return (
     <div>
-      <div>dashboard page (Protected)</div>
+      <div className="mb-8 space-y-4">
+        <div className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </div>
+        <div className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </div>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            key={tool.href}
+            onClick={() => router.push(tool.href)}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-cneter gap-x-4">
+              <tool.icon className={cn('p-2 w-fit rounded-md', tool.bgColor)} />
+              <div className="font-semibold">{tool.label}</div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
+      </div>
     </div>
   )
 }
